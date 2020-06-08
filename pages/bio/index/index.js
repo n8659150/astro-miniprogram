901184cc8c432f9f99b51ffc55e48f25075b35da
n8659150 <jackli900421@hotmail.com>
2020-06-08 22:16:20 +0800

    Add bio page - Aries and basic routing

--- pages/bio/index/index.js
+++ pages/bio/index/index.js
@@ -14,7 +14,55 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    elements: [{
+      title: '白羊座',
+      name: 'Aries',
+    },
+    {
+      title: '金牛座',
+      name: 'Aries',
+    },
+    {
+      title: '双子座',
+      name: 'Aries',
+    },
+    {
+      title: '巨蟹座',
+      name: 'Aries',
+    },
+    {
+      title: '狮子座',
+      name: 'Aries',
+    },
+    {
+      title: '处女座',
+      name: 'Aries',
+    },
+    {
+      title: '天秤座',
+      name: 'Aries',
+    },
+    {
+      title: '天蝎座',
+      name: 'Aries',
+    },
+    {
+      title: '射手座',
+      name: 'Aries',
+    },
+    {
+      title: '摩羯座',
+      name: 'Aries',
+    },
+    {
+      title: '水瓶座',
+      name: 'Aries',
+    },
+    {
+      title: '双鱼座',
+      name: 'Aries',
+    },
+  ],
   },
 
   /**
